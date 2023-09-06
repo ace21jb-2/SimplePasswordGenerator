db1c0ded25cd33bcfca4331f39089608055f2876
ace21jb-2 <103265489+ace21jb-2@users.noreply.github.com>
2023-09-06 11:47:28 +0100

    Update password generator.py
    Split character types into different generators to allow for different occurrences of different character types.

--- password generator.py
+++ password generator.py
@@ -2,11 +2,17 @@
 
 import random
 str = ""
-for i in range (4):
-    str = str + (chr(random.randint(48,57)))
+for i in range (4): #Uppercase letters
     str = str + (chr(random.randint(65,90)))
+
+for i in range (4): #Lowercase letters
     str = str + (chr(random.randint(97,122)))
 
-#str = str +(chr(random.randint(33,47)))
+for i in range (4): #Numbers
+    str = str + (chr(random.randint(48,57)))
+
+for i in range (0): #Special Characters
+    # ( !,",#,$,%,&,',(,),*,+,,,-,.,/ )
+    str = str +(chr(random.randint(33,47)))
 
 print (''.join(random.sample(str, len(str))))
